3cacddf63aed637b0afefcccaae4aa31188aaf19
arrizalfb <arrizalfauzanb@gmail.com>
2022-06-06 22:30:13 +0700

    Update Navbar.js
    Updating target="_blank"

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
             <div class="container">
-                <a class="navbar-brand" href="https://react-bootstrap.netlify.app/"><img src="assets/img/navbar-logo.svg" alt="..." target="_blank" /></a>
+                <a class="navbar-brand" href="https://react-bootstrap.netlify.app/" target="_blank"><img src="assets/img/navbar-logo.svg" alt="..." /></a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                     Menu
                     <i class="fas fa-bars ms-1"></i>
